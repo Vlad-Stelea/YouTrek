5a81f0d1542032446f6d7bf0691a7834cb39d4ac
emmettmccann <mccannemmett@gmail.com>
2019-11-21 15:04:28 -0500

    some small test edits

--- vue/xscratch-app/src/App.vue
+++ vue/xscratch-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div id="header">youTrek</div>
+    <router-view />
   </div>
 </template>
 
@@ -13,11 +13,18 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
+}
+
+#header {
+  width: 100%;
+  height: 60px;
+  background-color: #2c3e50;
 }
 </style>
