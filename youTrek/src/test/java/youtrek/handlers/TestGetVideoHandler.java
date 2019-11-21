5756bcd273629a484c5b959ee9b681dcb5e09719
Vlad-Stelea <vladstelea@gmail.com>
2019-11-20 21:35:55 -0500

    Added test for get video handler

---
+++ youTrek/src/test/java/youtrek/handlers/TestGetVideoHandler.java
@@ -0,0 +1,35 @@
+package youtrek.handlers;
+
+import org.junit.Before;
+import org.junit.Test;
+import youtrek.http.GetVideosRequest;
+import youtrek.http.GetVideosResponse;
+import youtrek.models.ListOfVideos;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+public class TestGetVideoHandler {
+    GetVideoHandler handler;
+    ListOfVideos lov;
+    GetVideosRequest request;
+
+    @Before
+    public void setup() {
+        handler = new GetVideoHandler();
+        lov = new ListOfVideos(new ArrayList<>());
+        request = new GetVideosRequest();
+    }
+
+    @Test
+    public void testSuccesfulResponse() {
+        GetVideosResponse result = handler.handleRequest(request, null);
+        assertEquals(result.getStatusCode(), 200);
+        assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+        String content = result.getBody();
+        assertNotNull(content);
+    }
+}
