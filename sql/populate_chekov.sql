d6fdd13fc6f66cf7ebf87e83ad392e893cb381ac
Connor McLaughlin <cjmclaughlin@wpi.edu>
2019-11-20 20:21:32 -0500

    Secrets Manager and Minor Schema Changes

---
+++ sql/populate_chekov.sql
@@ -0,0 +1,23 @@
+-- Add initial values to DB
+
+-- TLP Inserts
+-- Note that the first value (id=1) will refer to our local library (TODO discuss)
+INSERT INTO tlps (id, base_url) values (NULL, 'https://xscratch-videos.s3.us-east-2.amazonaws.com');
+
+
+-- Video Inserts (TODO after TA verifies our segments)
+-- Test video for initial DB test (TODO remove after getting the rest of them
+INSERT INTO videos (id, name, is_remote, is_available, url, dialogue, tcp_id) values (NULL, 'testVid', false, false, '/C2_Mental_Disease.ogg', 'Mental disease has affected the entire crew', NULL);
+
+
+-- Character Inserts
+-- TODO inserts for potential missing characters
+INSERT INTO characters (id, name) values (NULL, 'Chekov');
+INSERT INTO characters (id, name) values (NULL, 'Spock');
+INSERT INTO characters (id, name) values (NULL, 'Kirk');
+INSERT INTO characters (id, name) values (NULL, 'Uhura');
+INSERT INTO characters (id, name) values (NULL, 'Scotty');
+INSERT INTO characters (id, name) values (NULL, 'Bones');
+INSERT INTO characters (id, name) values (NULL, 'Sulu');
+
+
---
+++ youTrek/src/main/java/youtrek/db/KeyManager.java
@@ -0,0 +1,79 @@
+package youtrek.db;
+
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.Base64;
+
+public class KeyManager {
+
+    private final String[] secret;
+
+    KeyManager() {
+        secret = getConnectionInfo();
+    }
+
+    String getHost() {
+        if (secret == null) return null;
+        return secret[0];
+    }
+
+    String getUser() {
+        if (secret == null) return null;
+        return secret[1];
+    }
+
+    String getPass() {
+        if (secret == null) return null;
+        return secret[2];
+    }
+
+
+    private static String getSecret() {
+        String secretName = "ChekovDB";
+        String region = "us-east-2";
+
+        AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
+                .withRegion(region)
+                .build();
+
+        String secret, decodedBinarySecret;
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = null;
+
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (DecryptionFailureException | InternalServiceErrorException | InvalidParameterException | InvalidRequestException | ResourceNotFoundException e) {
+            throw e;
+        }
+
+        if (getSecretValueResult.getSecretString() != null) {
+            secret = getSecretValueResult.getSecretString();
+            return(secret);
+        }
+        else {
+            decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+            return(decodedBinarySecret);
+        }
+    }
+
+    private String[] getConnectionInfo() {
+        String secret = getSecret();
+        if (secret == null) {
+            return null;
+        }
+        else {
+            String[] secretArray = new String[3];
+            JsonObject jo = new JsonParser().parse(secret).getAsJsonObject();
+            secretArray[0] = jo.get("host").getAsString();
+            secretArray[1] = jo.get("username").getAsString();
+            secretArray[2] = jo.get("password").getAsString();
+            return secretArray;
+        }
+    }
+
+}
