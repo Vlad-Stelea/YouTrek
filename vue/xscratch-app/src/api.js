8004a76a585ac44219ab36c8945cae6980761594
emmettmccann <mccannemmett@gmail.com>
2019-11-24 15:25:30 -0500

    v1 done w/o bootstrap

---
+++ vue/xscratch-app/src/api.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'https://fv9h3awn2g.execute-api.us-east-2.amazonaws.com/beta'
+})
+
+export default {
+  async execute (method, resource, data) {
+    return client({
+      method: method,
+      url: resource,
+      data: data
+    }).then(req => {
+      return req
+    }).catch((error) => {
+      new Vue().$bvToast.toast(resource, {
+        title: 'Error',
+        variant: 'danger',
+        solid: true,
+        autoHideDelay: 2000,
+        isStatus: true,
+        noCloseButton: true,
+        toaster: 'b-toaster-top-right'
+      })
+      return error.response
+    })
+  },
+
+  async getVideos () {
+    const response = await this.execute('get', '/videos')
+    return JSON.parse(response.data.body).videos
+  },
+
+  async getPlaylists () {
+    const response = await this.execute('get', '/playlists')
+    return JSON.parse(response.data.body).playlists
+  }
+}
