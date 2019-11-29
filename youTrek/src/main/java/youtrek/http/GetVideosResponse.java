f2eea70c7cda21a41c11bc09bb53123f900a30f3
Vlad Stelea <vladstelea@gmail.com>
2019-11-29 12:11:35 -0500

    Refractored http responses as they had a lot of shared code (#19)

--- youTrek/src/main/java/youtrek/http/GetVideosResponse.java
+++ youTrek/src/main/java/youtrek/http/GetVideosResponse.java
@@ -1,41 +1,13 @@
 package youtrek.http;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import youtrek.models.ListOfVideos;
 
-import youtrek.models.Video;
-import youtrek.models.adapters.ListOfVideosAdapter;
-
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
-public class GetVideosResponse {
-    final ListOfVideos videos;
-    private final Map<String, String> headers;
-    private final int statusCode;
+public class GetVideosResponse extends AbstractYouTrekResponse<ListOfVideos> {
 
     public GetVideosResponse(ListOfVideos videos, final Map<String, String> headers, final int statusCode) {
-        this.videos = videos;
-        this.statusCode = statusCode;
-        this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+        super(videos, headers, statusCode);
     }
-
-    public String getBody() {
-        Gson gson = new GsonBuilder().
-                //registerTypeAdapter(ListOfVideos.class, new ListOfVideosAdapter()).
-                create();
-        return gson.toJson(this.videos);
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
 
 }
