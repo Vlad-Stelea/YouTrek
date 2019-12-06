f4f8d11ad7b21a683a9e6f1efd44d81500df2f9d
Vlad Stelea <vladstelea@gmail.com>
2019-12-05 20:03:08 -0500

    Added base64 decoding (#33)

--- youTrek/src/test/java/youtrek/s3/TestS3Util.java
+++ youTrek/src/test/java/youtrek/s3/TestS3Util.java
@@ -3,17 +3,17 @@ package youtrek.s3;
 import static org.junit.Assert.*;
 
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.util.Base64;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class TestS3Util {
     S3Util s3Util;
-    String base64String = "W4p5qHNrgMb5iOl4oub7mK2t6/6G11rNRpl1IsLGZ73oqon2vFN9OKHYvAP7FPhbv1cPixiq7mkVMTRuAKFj";
+    String testFileString = "TestString";
     String testingBucketName = "s3-utils-test-bucket";
     String testFileName = "Test File";
 
@@ -24,21 +24,23 @@ public class TestS3Util {
 
     @Test
     public void testConvertBase64StringToInputStreamIsLossless() throws IOException {
-        InputStream is = s3Util.convertBase64StringToStream(base64String);
-        String decodedString = getStringInputStream(is);
-        assertEquals(base64String, decodedString);
+        String encodedString = Base64.encodeAsString(testFileString.getBytes());
+        InputStream is = s3Util.decodeAndConvertBase64StringToStream(encodedString);
+        String decodedString = getStringFromInputStream(is);
+        assertEquals(testFileString, decodedString);
     }
 
     @Test
     public void testPutVideoToBucket() throws Exception {
         //If uploadVideoFails, test will fail anyways because of exception
-        PutObjectResult result = s3Util.uploadVideoToBucket(testingBucketName, testFileName, base64String);
+        String encodedString = Base64.encodeAsString(testFileString.getBytes());
+        PutObjectResult result = s3Util.uploadVideoToBucket(testingBucketName, testFileName, encodedString);
         assertNotNull(result);
         //Delete the uploaded file to not use up space for no reason
         s3Util.deleteFile(testingBucketName, testFileName);
     }
 
-    private String getStringInputStream(InputStream is) {
+    private String getStringFromInputStream(InputStream is) {
         StringBuffer parsedStringBuffer = new StringBuffer();
         Scanner sc = new Scanner(is);
         while(sc.hasNext()) parsedStringBuffer.append(sc.next());
