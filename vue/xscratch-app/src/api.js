3abea903352835f18c21eb4b6426df39d6dba90a
emmettmccann <mccannemmett@gmail.com>
2019-12-07 22:33:12 -0500

    linting fix

--- vue/xscratch-app/src/api.js
+++ vue/xscratch-app/src/api.js
@@ -86,12 +86,12 @@ export default {
     const response = await this.execute('get', '/tlp')
     return JSON.parse(response.data.body).listOfTLP
   },
-  
+
   async deleteTLP (id) {
     const response = await this.execute('post', '/tlp/delete', id)
     return JSON.parse(response.data.body)
   },
-  
+
   async appendVideo (playlistID, videoID) {
     const body = {
       'id': videoID
